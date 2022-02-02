7f95a09ec9fb87a38e830add7b0e3b7def5a531c
Karan Udayan Das <karanudayandas@gmail.com>
2022-02-02 18:04:37 +0530

    Update subset sum equal to k

--- DP/Subset Sum Equal To K.cpp
+++ DP/Subset Sum Equal To K.cpp
@@ -72,3 +72,23 @@ bool subsetSumToK(int n, int k, vector<int> &arr) {
     vector<vector<int>> dp(n, vector<int>(k+1, -1));
     return solve(n-1, k, arr, dp);
 }
+
+// TC - O(n*k) SC - O(n*k)
+
+bool subsetSumToK(int n, int k, vector<int> &arr) {
+    // Write your code here.
+    vector<vector<bool>> dp(n, vector<bool>(k+1, 0));
+    //return solve(n-1, k, arr, dp);
+    for(int idx=0; idx<n; idx++) dp[idx][0] = true;
+    dp[0][arr[0]] = true;
+    for(int idx=1; idx<n; idx++){
+        for(int target = 1; target<=k; target++){
+            bool notTake = dp[idx-1][target];
+            bool take = false;
+            if(target >= arr[idx])
+                take = dp[idx-1][target-arr[idx]];
+            dp[idx][target] = notTake || take;
+        }
+    }
+    return dp[n-1][k];
+}
